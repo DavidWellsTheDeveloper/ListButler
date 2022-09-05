c5a2ba2628d3402d155bf0a625b36ec68a5c5395
dave <dave1.t.wells@gmail.com>
2022-09-05 11:41:39 -0600

    add static root

--- api/api/settings.py
+++ api/api/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/' # add this
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # add this
