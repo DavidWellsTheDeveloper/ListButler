55994d61b03f32efa639e5bbf150c0c50d5172ca
dave <dave1.t.wells@gmail.com>
2022-10-09 14:29:11 -0600

    frontend folder

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    'jest/globals': true,
     node: true
   },
   parserOptions: {
@@ -13,6 +14,7 @@ module.exports = {
     'plugin:nuxt/recommended'
   ],
   plugins: [
+    'jest'
   ],
   // add your custom rules here
   rules: {}
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client',
+    title: 'frontend',
     htmlAttrs: {
       lang: 'en'
     },
@@ -44,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000/api'
+    baseURL: '/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
